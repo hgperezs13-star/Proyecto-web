b871ace528c3de06b24f172a2eb3d526e46aa87c
hgperezs13-star <hgperezs13@gmail.com>
2026-09-16 22:12:02 -0300

    Add files via upload

--- web-1/script.js
+++
@@ -1,8 +0,0 @@
-gsap.registerPlugin(ScrollTrigger);
-const cards = document.querySelectorAll('.card');
-document.addEventListener('DOMContentLoaded', (event) => {
-    cards.forEach((card, index) => {
-        gsap.from(`.card${index}`,
-            { y: 30, x: (index % 2 === 0 ? 30 : -30), opacity: 0, scrollTrigger: { trigger:`.card${index}`, end: 'top center', scrub: true } })
-    })
-})
